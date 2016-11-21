64c4561e765191d6406f334118293cdf73aadd73
dg96 <dhairyagandhi96@gmail.com>
2016-11-21 17:48:47 +0530

    first commit

---
+++ static/server.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import MySQLdb
+try:
+  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+  from SocketServer import TCPServer as Server
+except ImportError:
+  from http.server import SimpleHTTPRequestHandler as Handler
+  from http.server import HTTPServer as Server
+
+# Read port selected by the cloud for our application
+PORT = int(os.getenv('PORT', 8000))
+
+mat = np.matrix([1,2],[3,2])
+print mat
+
+# Change current directory to avoid exposure of control files
+os.chdir('static')
+
+httpd = Server(("", PORT), Handler)
+try:
+  print("Start serving at port %i" % PORT)
+  httpd.serve_forever()
+except KeyboardInterrupt:
+  pass
+httpd.server_close()
+
